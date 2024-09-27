b63aae2f53ec35a5438032dc8c843a730db9cdbc
Bacari Hersey <herseyb71@gmail.com>
2024-09-27 07:46:07 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Metroid-Site
+
+go 1.22.2
---
+++ src/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var templ *template.Template
+
+func init() {
+	templ = template.Must(template.ParseFiles("./templates/index.html"))
+}
+
+// start here and see if you can get the backend connected to this and sen setup htmx frontend
+func main() {
+	fmt.Println(specificSamusAbility())
+
+}
+
+func specificSamusAbility() string {
+	resp, err := http.Get("https://localhost:7149/api/Trackers/1")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyString := string(bodyBytes)
+	errorHandler := templ.Execute(os.Stdout, bodyString)
+	if errorHandler != nil {
+		log.Fatal(errorHandler)
+	}
+
+	return bodyString
+	// fmt.Println(resp.StatusCode)
+	// fmt.Println(resp.Header)
+	// fmt.Println(resp.Header["Content-Type"])
+	// fmt.Println(resp.Header["Content-Type"][0])
+	// fmt.Println(bodyString)
+}
